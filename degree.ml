a3237e373199e2d74457f7418f89edea73bac749
theteachr <sop.theteachr@gmail.com>
2023-06-04 10:41:27 +0530

    YAR

--- degree.ml
+++ degree.ml
@@ -1,4 +1,4 @@
-type indexed_count = { first_at : int; last_at : int; count : int }
+type indexed_count = { first_at : int; length : int; count : int }
 
 module IndexedCount = Map.Make (Int)
 
@@ -8,17 +8,19 @@ let build_knowledge nums =
     let value =
       match IndexedCount.find_opt n data with
       | Some v -> v
-      | None -> { first_at = idx; last_at = idx; count = 0 }
+      | None -> { first_at = idx; length = 0; count = 0 }
     in
     let new_count = value.count + 1 in
-    ( Int.max curr_max new_count,
-      IndexedCount.add n { value with last_at = idx; count = new_count } data )
+    let updated_value =
+      { value with length = idx - value.first_at + 1; count = new_count }
+    in
+    (Int.max curr_max new_count, IndexedCount.add n updated_value data)
   in
   List.fold_left aux (0, IndexedCount.empty) indexed |> Option.some
 
 let print_entry n value =
-  Printf.sprintf "%d: (first_at: %d, last_at: %d) [%d]" n value.first_at
-    value.last_at value.count
+  let { first_at; length; count } = value in
+  Printf.sprintf "%d: (first_at: %d, length: %d) [%d]" n first_at length count
   |> print_endline
 
 let process (degree, knowledge) =
@@ -32,9 +34,8 @@ let process (degree, knowledge) =
   | first :: rest ->
       List.fold_left_map
         (fun acc n ->
-          let { first_at; last_at } = IndexedCount.find n knowledge in
-          let subarray_len = last_at - first_at + 1 in
-          (Int.min acc subarray_len, subarray_len))
+          let { length } = IndexedCount.find n knowledge in
+          (Int.min acc length, length))
         first most_frequent
       |> Option.some
   | [] -> None
@@ -44,7 +45,7 @@ let test = [ 1; 2; 2; 1; 3 ]
 let () =
   match build_knowledge test with
   | Some (_, data) -> IndexedCount.iter print_entry data
-  | None -> print_endline "¯\_(ツ)_/¯"
+  | None -> print_endline "¯\\_(ツ)_/¯"
 
 let solve =
   let res = test |> build_knowledge in
