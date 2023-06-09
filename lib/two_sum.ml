eb7af12227ff656f385ebf76d9faef3270752abf
theteachr <sop.theteachr@gmail.com>
2023-06-10 03:27:33 +0530

    Solve 1 - Two Sum

--- lib/two_sum.ml
+++ lib/two_sum.ml
@@ -1,6 +1,23 @@
 type input = int list * int
 type output = int * int
 
-let parse _ = ([1; 2; 3], 2)
-let to_string (first, snd) = Printf.sprintf "%d %d" first snd
-let solve _ = (0, 0)
+module Index_map = Map.Make (Int)
+
+let parse lines =
+  let to_nums line = String.split_on_char ' ' line |> List.map int_of_string in
+  match String.split_on_char '\n' lines with
+  | [ nums_line; target ] -> (to_nums nums_line, int_of_string target)
+  | _ -> failwith "Malformed input"
+
+let to_string (f, s) = Printf.sprintf "%d %d" f s
+
+let solve (nums, target) =
+  let rec aux acc = function
+    | [] -> Index_map.iter (fun n i -> Printf.sprintf "n = %d; i = %d" n i |> print_endline) acc;failwith "The problem statement lied"
+    | (i, n) :: t -> begin
+        match Index_map.find_opt (n - target |> abs) acc with
+        | Some i' -> (i', i) (* TODO: Make sure i' < i *)
+        | _ -> aux (Index_map.add n i acc) t
+    end
+  in
+  aux Index_map.empty (nums |> List.mapi (fun i n -> (i, n)))
