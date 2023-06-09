1488e9613da71d8353556b85ece3874eaeefeed3
theteachr <sop.theteachr@gmail.com>
2023-06-10 02:51:37 +0530

    Solve 20 - Balanced Brackets

--- lib/bal_parens.ml
+++ lib/bal_parens.ml
@@ -1,7 +1,36 @@
-type input = string
+type bracket = Round | Curly | Square
+type opening_bracket = Opening of bracket
+
+module Bracket_state = struct
+  type t = Open of bracket | Close of bracket
+
+  let of_char = function
+    | '(' -> Some (Open Round)
+    | '{' -> Some (Open Curly)
+    | '[' -> Some (Open Square)
+    | ')' -> Some (Close Round)
+    | '}' -> Some (Close Curly)
+    | ']' -> Some (Close Square)
+    | _ -> None
+end
+
+type input = Bracket_state.t list
 type output = bool
 
-let parse = Fun.id
+let parse line =
+  line
+  |> String.to_seq
+  |> List.of_seq
+  |> List.filter_map Bracket_state.of_char
+
 let to_string = Bool.to_string
 
-let solve _ = false
+let solve brackets =
+  let open Bracket_state in
+  let rec aux input st = match (input, st) with
+    | [], [] -> true
+    | Open b :: bs, _ -> aux bs ((Opening b) :: st)
+    | Close b :: bs, Opening b' :: bs' when b = b' -> aux bs bs'
+    | _ -> false
+  in
+  aux brackets []
