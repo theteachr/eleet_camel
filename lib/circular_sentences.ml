c7159576405e4c3236a19e217ced5b52b94a7b71
theteachr <sop.theteachr@gmail.com>
2023-06-19 13:34:50 +0530

    Fix solution for Circular Sentences

--- lib/circular_sentences.ml
+++ lib/circular_sentences.ml
@@ -11,9 +11,16 @@ let last_char s = String.length s - 1 |> String.get s
 let first_char s = String.get s 0
 
 let solve test =
+  let words = String.split_on_char ' ' test in
+  (* Just keep the _fi_rst and _la_st character_s_ of every word. *)
+  let filas = List.map (fun w -> (first_char w, last_char w)) words in
+  (* Get the first character of the first word to compare it later
+     with the last character of the last word. *)
+  let first, _ = List.hd filas in
   let rec is_circular = function
-    | [] | [ _ ] -> true
-    | x :: x' :: rest when last_char x = first_char x' -> is_circular rest
+    | [] -> true
+    | [ (_, c) ] -> c = first
+    | (_, x) :: ((x', _) :: _ as rest) when x = x' -> is_circular rest
     | _ -> false
   in
-  String.split_on_char ' ' test |> is_circular
+  is_circular filas
