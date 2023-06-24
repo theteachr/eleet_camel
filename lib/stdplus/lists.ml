1dfd6166b6ce283e942069fa5c116cd159580f8b
theteachr <sop.theteachr@gmail.com>
2023-06-24 18:57:31 +0530

    Use `pairs` instead of `acc`

--- lib/stdplus/lists.ml
+++ lib/stdplus/lists.ml
@@ -1,10 +1,10 @@
 let enumerate lst = List.mapi (fun i x -> (i, x)) lst
 
 let zip_longest l_one l_two ~default =
-  let rec zip acc = function
-    | [], [] -> List.rev acc
-    | x :: xs, y :: ys -> zip ((x, y) :: acc) (xs, ys)
-    | x :: xs, [] -> zip ((x, default) :: acc) (xs, [])
-    | [], y :: ys -> zip ((default, y) :: acc) ([], ys)
+  let rec zip pairs = function
+    | [], [] -> List.rev pairs
+    | x :: xs, y :: ys -> zip ((x, y) :: pairs) (xs, ys)
+    | x :: xs, [] -> zip ((x, default) :: pairs) (xs, [])
+    | [], y :: ys -> zip ((default, y) :: pairs) ([], ys)
   in
   zip [] (l_one, l_two)
