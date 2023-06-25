2b4148fbe2e1760a183ecd5180e4c0c721dcf62b
theteachr <sop.theteachr@gmail.com>
2023-06-25 20:32:20 +0530

    Implement `insert`

--- lib/prefix_tree.ml
+++ lib/prefix_tree.ml
@@ -16,6 +16,41 @@ module Command = struct
     | _ -> None
 end
 
+let char_index c = Char.code c - Char.code 'a'
+
+module Prefix_tree = struct
+  type t =
+    { ends : bool
+    ; paths : t option array
+    }
+
+  let empty_paths = Array.make 26 None
+
+  let empty = { ends = false; paths = empty_paths }
+
+  let insert s tree =
+    let rec ins chars tree =
+      match (tree, chars) with
+      | ({ paths; _ } as node), c :: [] ->
+          let idx = char_index c in
+          let path_node =
+            try Array.get paths idx with Invalid_argument _ -> Some empty
+          in
+          Array.set paths idx { path_node with ends = true };
+          node
+      | ({ paths; _ } as node), c :: cs ->
+          let new_node = ins cs empty in
+          Array.set paths (char_index c) (Some new_node);
+          node
+      | node, [] -> node
+    in
+    ins (s |> String.to_seq |> List.of_seq) tree
+
+  let search _ _ = false
+
+  let starts_with _ _ = false
+end
+
 type input = Command.t list
 
 type cmd_out =
