c7064c650308f813aab21a053cd1e6ac529c4a15
theteachr <sop.theteachr@gmail.com>
2023-07-08 18:43:01 +0530

    Finally rob houses the right way

--- lib/house_robber.ml
+++ lib/house_robber.ml
@@ -6,10 +6,8 @@ let parse line = line |> String.split_on_char ' ' |> List.map int_of_string
 
 let to_string = Int.to_string
 
-let rec rob pp p = function
-  | [] -> p
-  | [ h ] -> Int.max (pp + h) p
-  | [ h1; h2 ] -> Int.max (h1 + pp) (h2 + p) |> Int.max (h2 + pp)
-  | h1 :: h2 :: houses -> rob (h1 + pp) (h2 + p) houses
+let rec rob prev curr = function
+  | [] -> Int.max prev curr
+  | h :: houses -> rob curr (Int.max curr (h + prev)) houses
 
 let solve = rob 0 0
