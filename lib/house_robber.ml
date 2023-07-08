2aef104e730384d835611983ec78141e2194707d
theteachr <sop.theteachr@gmail.com>
2023-07-08 13:39:19 +0530

    Rob houses with optimal resource consumption
    Solve problem 198 (House Robber) optimally

--- lib/house_robber.ml
+++ lib/house_robber.ml
@@ -6,15 +6,10 @@ let parse line = line |> String.split_on_char ' ' |> List.map int_of_string
 
 let to_string = Int.to_string
 
-let rec rob_max sum = function
-  | [] -> sum
-  | [ h ] -> sum + h
-  | [ h1; h2 ] -> sum + Int.max h1 h2
-  | [ h1; h2; h3 ] -> sum + Int.max (h1 + h3) h2
-  | h1 :: h2 :: h3 :: (_ :: houses as rest) ->
-      let sum_one = rob_max (sum + h1 + h3) houses in
-      let sum_two = rob_max (sum + h2) rest in
-      let sum_three = rob_max (sum + h1) rest in
-      Int.max sum_one sum_two |> Int.max sum_three
+let rec rob_max sum_pp sum_p = function
+  | [] -> sum_p
+  | [ h ] -> sum_pp + h
+  | [ h1; h2 ] -> Int.max (h1 + sum_pp) (h2 + sum_p) |> Int.max (h2 + sum_pp)
+  | h1 :: h2 :: houses -> rob_max (h1 + sum_pp) (h2 + sum_p) houses
 
-let solve = rob_max 0
+let solve = rob_max 0 0
