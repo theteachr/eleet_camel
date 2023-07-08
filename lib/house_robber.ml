a522e04901fb0cf811a512ef2df657c89fdb016f
theteachr <sop.theteachr@gmail.com>
2023-07-08 13:40:46 +0530

    Shorter names :)

--- lib/house_robber.ml
+++ lib/house_robber.ml
@@ -6,10 +6,10 @@ let parse line = line |> String.split_on_char ' ' |> List.map int_of_string
 
 let to_string = Int.to_string
 
-let rec rob_max sum_pp sum_p = function
-  | [] -> sum_p
-  | [ h ] -> sum_pp + h
-  | [ h1; h2 ] -> Int.max (h1 + sum_pp) (h2 + sum_p) |> Int.max (h2 + sum_pp)
-  | h1 :: h2 :: houses -> rob_max (h1 + sum_pp) (h2 + sum_p) houses
+let rec rob pp p = function
+  | [] -> p
+  | [ h ] -> pp + h
+  | [ h1; h2 ] -> Int.max (h1 + pp) (h2 + p) |> Int.max (h2 + pp)
+  | h1 :: h2 :: houses -> rob (h1 + pp) (h2 + p) houses
 
-let solve = rob_max 0 0
+let solve = rob 0 0
