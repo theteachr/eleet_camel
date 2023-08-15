73d24d6077fbe1c957b809db043a847313807fcf
theteachr <sop.theteachr@gmail.com>
2023-08-15 16:21:53 +0530

    Update Matrix functions

--- lib/stdplus/matrix.ml
+++ lib/stdplus/matrix.ml
@@ -1,7 +1,7 @@
 type 'a t =
-{ items: 'a array array
-; dimensions: int * int
-}
+  { items : 'a array array
+  ; dimensions : int * int
+  }
 
 let of_list l =
   let items = Array.of_list l |> Array.map Array.of_list in
@@ -9,8 +9,16 @@ let of_list l =
   let num_cols = List.(length (hd l)) in
   { items; dimensions = (num_rows, num_cols) }
 
-let at (row, col) m = try Some m.items.(row).(col) with Invalid_argument _ -> None
+let at (row, col) m =
+  try Some m.items.(row).(col) with Invalid_argument _ -> None
 
 let at_exn (row, col) m = m.items.(row).(col)
 
 let dimensions m = m.dimensions
+
+let print_row (row : string array) =
+  String.concat " " (Array.to_list row) |> print_endline
+
+let map f m = { m with items = Array.map (Array.map f) m.items }
+
+let print m = Array.iter print_row m.items
--- lib/stdplus/matrix.mli
+++ lib/stdplus/matrix.mli
@@ -7,3 +7,7 @@ val at : int * int -> 'a t -> 'a option
 val at_exn : int * int -> 'a t -> 'a
 
 val dimensions : 'a t -> int * int
+
+val map : ('a -> 'b) -> 'a t -> 'b t
+
+val print : string t -> unit
