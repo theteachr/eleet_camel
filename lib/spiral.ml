ccc64b3fb3f1276848c713c991ccd17f569a87f4
theteachr <sop.theteachr@gmail.com>
2023-08-19 01:58:43 +0530

    :mute: Remove debug noise

--- lib/spiral.ml
+++ lib/spiral.ml
@@ -20,19 +20,14 @@ module Direction = struct
     | Right -> Down
     | Down -> Left
     | Left -> Up
-
-  let to_string = function
-    | Up -> "up"
-    | Right -> "right"
-    | Down -> "down"
-    | Left -> "left"
 end
 
 module Point = struct
   type t = int * int
 
+  open Direction
+
   let next direction (row, col) =
-    let open Direction in 
     match direction with
     | Up -> (row - 1, col)
     | Right -> (row, col + 1)
@@ -40,7 +35,6 @@ module Point = struct
     | Left -> (row, col - 1)
 
   let prev direction (row, col) =
-    let open Direction in 
     match direction with
     | Up -> (row + 1, col)
     | Right -> (row, col - 1)
@@ -56,25 +50,19 @@ module Cell = struct
   let unvisited x = Unvisited x
 end
 
-let solve (matrix : input) =
+let solve matrix =
   let matrix = Matrix.map Cell.unvisited matrix in
-  let rec spiral visited moving ((row, col) as loc) =
-    let open Printf in
-    printf "At: %d, %d\n" row col;
-    printf "Moving %s\n" @@ Direction.to_string moving;
+  let rec spiral visited moving loc =
     let value_opt, new_direction, new_loc =
       match Matrix.at loc matrix with
       | Some (Unvisited _ as x) -> (Some x, moving, loc)
       | _ ->
           let new_direction = Direction.next moving in
-          printf "Changing direction to: %s\n" @@ Direction.to_string new_direction;
-          let ((row, col) as new_loc) = Point.next new_direction (Point.prev moving loc) in
-          printf "New location: %d, %d\n" row col;
+          let new_loc = Point.next new_direction (Point.prev moving loc) in
           (Matrix.at new_loc matrix, new_direction, new_loc)
     in
     match value_opt with
     | Some (Unvisited value) ->
-        printf "Value found: %d\n" value;
         Matrix.update new_loc Cell.Visited matrix;
         let new_loc = Point.next new_direction new_loc in
         spiral (value :: visited) new_direction new_loc
